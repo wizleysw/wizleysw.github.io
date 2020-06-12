d22c98bcea235f733c9fbcacb1b97a158f38450a
wizleysw <wizley@kakao.com>
2020-06-12 14:42:22 +0900

    add new category android

---
+++ _pages/category-android.md
@@ -0,0 +1,6 @@
+---
+title: "안드로이드 일지"
+layout: category-android
+permalink: /android/
+author_profile: true
+---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,26 @@
 var store = [{
-        "title": "HelloWorld!",
-        "excerpt":"HelloWorld!   HelloWorld!  ","categories": ["TEST"],
-        "tags": ["INIT"],
-        "url": "http://localhost:4000/test/HelloWorld/",
+        "title": "Hello Design Pattern!",
+        "excerpt":"HelloWorld! 위즐리의 개발 블로그이다. 해보고 싶었던 개발, 해보고 싶었던 개발과 관련된 내용들을 담을 예정이다. 해킹에 관련된 내용을 담을지는 모르겠다. 현재를 기준으로는 개발이 대부분을 차지하지 않나 싶다. 연구한 항목이 많지만 적히게 될 대부분의 내용은 해오던 것이 아닌 해보고 싶었던 것 위주가 될 듯하다. 위즐리의 블로그 시작! Design Pattern 2019년도는 C++로 작성된...","categories": ["dev"],
+        "tags": ["design_pattern","algorithm"],
+        "url": "http://localhost:4000/dev/Hello-Design-Pattern/",
         "teaser":null},{
         "title": "PHP + nginx + mysql @ docker로 게시판 구현하기",
-        "excerpt":"Overview 이번 프로젝트의 목표는 크게 3가지로 나눌 수 있을 것 같다. 첫 번째 목표는 웹개발을 해본다는 것이다. PHP는 대학교를 첫 입학했던 시기에는 가장 많이 사용하는 웹 개발 언어였다. 하지만 지금은 React, Django 등의 더 사용하기 쉽고 강력한 프레임웍들이 나왔고 php는 개발자들이 유지보수에 적극적이지 않다는 이유로 점차 사용되지 않는 언어가 되어가고...","categories": ["dev"],
-        "tags": ["PHP"],
+        "excerpt":"Overview 이번 프로젝트의 목표는 크게 3가지로 나눌 수 있을 것 같다. 첫 번째 목표는 웹개발을 해본다는 것이다. PHP는 대학교를 첫 입학했던 시기에는 가장 많이 사용하는 웹 개발 언어였다. 하지만 지금은 React, Django 등의 더 사용하기 쉽고 강력한 프레임웍들이 나왔고 PHP의 개발자들이 단점 보완에 적극적이지 않다는 이유등으로 점차 사용되지 않는 언어가...","categories": ["dev"],
+        "tags": ["PHP","docker","nginx"],
         "url": "http://localhost:4000/dev/PHP-+-nginx-+-mysql-@-docker%EB%A1%9C-%EA%B2%8C%EC%8B%9C%ED%8C%90-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0/",
+        "teaser":null},{
+        "title": "Django + REST + SQLite @ docker로 Eaten_Away 서비스 개발하기",
+        "excerpt":"Overview 방금 PHP 개발 프로젝트를 마무리하고 바로 시작하게 된 다음 프로젝트는 요즘 젤 잘 나가는 강력한 프레임워크인 Django를 사용한다. 그리고 이전 포스팅은 간단한 게시판을 구현한 정도였다면 이번에는 하나의 서비스를 개발한다는 생각으로 진행할 예정이다. 이번에도 docker를 사용할 것인데 도커 이외에 Django + REST + SQLite라는 또다시 나에게 생소한 조합으로 개발을 진행하고자...","categories": ["dev"],
+        "tags": ["Django","REST","docker"],
+        "url": "http://localhost:4000/dev/Django-+-REST-+-SQLite-@-docker%EB%A1%9C-Eaten_Away-%EC%84%9C%EB%B9%84%EC%8A%A4-%EA%B0%9C%EB%B0%9C%ED%95%98%EA%B8%B0/",
+        "teaser":null},{
+        "title": "JAVA + graphQL로 안드로이드 어플 클론코딩하기",
+        "excerpt":"Overview 장고로 프로젝트가 마무리되자 말자 선택한 프로젝트는 안드로이드 클론 코딩이다. 개발자들이 많이 쓰는 JAVA라는 언어에 대해서 전혀 몰랐기 때문에 나중에 Spring으로 개발을 진행하는 상황을 대비할겸, 가장 핫한 주제인 모바일에 관련된 어플을 만들어보고싶기도했다. 사실 안드로이드를 개발해본적은 없지만 해킹 및 안드로이드 어플을 리버싱하는 과정을 통해서 대략적으로 어떻게 생겼는지에 대한 정보는 알고 있지만...","categories": ["dev"],
+        "tags": ["Android","graphQL","JAVA"],
+        "url": "http://localhost:4000/dev/JAVA-+-graphQL%EB%A1%9C-%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%96%B4%ED%94%8C-%ED%81%B4%EB%A1%A0%EC%BD%94%EB%94%A9%ED%95%98%EA%B8%B0/",
+        "teaser":null},{
+        "title": "너의 이름은 영화에 나온 MyDiary를 구현한 오픈소스 앱 똑같이 따라 코딩해보기",
+        "excerpt":"Overview 두 달이라는 기간동안 안드로이드 어플리케이션을 개발하기 위해 열중하였고 결국 aintstagram이라는 완성본을 만들게 되었다. 하지만 구현을 하는 도중에 이게 최선의 방법일까라는 고민을 수도 없이 하였고 좀 더 좋은 방법이 분명 존재할 것 같다는 생각을 자주 하게 되었다. 그래서 이번에는 다른 개발자가 구현한 앱의 코드 흐름을 그대로 따라가면서 좀 더 효율적인...","categories": ["dev"],
+        "tags": ["Android","JAVA"],
+        "url": "http://localhost:4000/dev/%EB%84%88%EC%9D%98-%EC%9D%B4%EB%A6%84%EC%9D%80-%EC%98%81%ED%99%94%EC%97%90-%EB%82%98%EC%98%A8-MyDiary%EB%A5%BC-%EA%B5%AC%ED%98%84%ED%95%9C-%EC%98%A4%ED%94%88%EC%86%8C%EC%8A%A4-%EC%95%B1-%EB%98%91%EA%B0%99%EC%9D%B4-%EB%94%B0%EB%9D%BC-%EC%BD%94%EB%94%A9%ED%95%B4%EB%B3%B4%EA%B8%B0/",
         "teaser":null}]
